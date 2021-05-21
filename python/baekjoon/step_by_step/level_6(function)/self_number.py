34a6583da918c652f165c4463ba020dec117d726
WonJune Kang <devsigner9920@icloud.com>
2021-05-21 23:05:01 +0900

    modified

--- python/baekjoon/step_by_step/level_6(function)/self_number.py
+++ python/baekjoon/step_by_step/level_6(function)/self_number.py
@@ -1,13 +1,1 @@
-def d(n):
-    a = n
-    if a < len(arr):
-        arr[a] = False
-        a = a + sum(list(map(int, str(a))))
-        return d(a)
-
-
-arr = [True] * 10000
-for i in range(len(arr)):
-    d(i + 1)
-
-
+print('to be continued')
