060506a96d49bb6561e1b5315dd4a6f7b8b831e6
WonJune Kang <devsigner9920@icloud.com>
2021-07-07 23:36:56 +0900

    goldbachs solved

--- python/baekjoon/step_by_step/level_9/goldbachs_conjecture.py
+++ python/baekjoon/step_by_step/level_9/goldbachs_conjecture.py
@@ -10,7 +10,7 @@ for i in range(2, int(num ** 0.5) + 1):
             seive[j] = False
 
 seive_num_list = [i for i, val in enumerate(seive) if val]
-print(seive_num_list)
+
 for _ in range(t):
     n = int(input())
 
@@ -22,10 +22,11 @@ for _ in range(t):
     ans_n = 0
     ans_m = 9999
     for k in seive_num_list:
+        if n < k: break
         ans = n - k
         if seive[ans]:
            if abs(ans_n - ans_m) >= abs(k - ans):
-               ans_n = k
-               ans_m = ans
+               ans_n = ans
+               ans_m = k
     
     print(ans_n, ans_m)
