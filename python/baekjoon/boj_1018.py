7901e678e6386d9dc7cc092b5dab65c31b0cfa46
devsigner <devsigner9920@icloud.com>
2021-07-31 23:07:32 +0900

    commit

---
+++ python/baekjoon/boj_1018.py
@@ -0,0 +1,27 @@
+"""
+8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+"""
+
+from sys import stdin
+
+n, m = map(int, stdin.readline().rstrip().split())
+chess = [stdin.readline().rstrip() for _ in range(n)]
+
+test = ''
+for i in range(n - 7):
+    for j in range(m - 7):
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
+                test += chess[k][l]
+            test += '\n'
+        test += '==========================\n'
+
+print(test)
