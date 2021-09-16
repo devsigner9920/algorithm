31e117887c42524bfddd591dc0c114922fa8d508
devsigner <devsigner9920@icloud.com>
2021-09-16 22:57:17 +0900

    commit

--- python/baekjoon/boj_11729.py
+++ python/baekjoon/boj_11729.py
@@ -1,13 +1,13 @@
-def hanoi(n, frm, to, sub):
-    if n == 1:
-        print(frm, to)
-        return
+def hanoi(n, a, b):
+    if n > 1:
+        hanoi(n-1, a, 6-a-b)              # 기둥이 1개 이상이면 그룹으로 묶인 n-1개 원판을
+                                          # 중간으로 먼저 다 옮긴다
+    print(a, b)
 
-    hanoi(n - 1, frm, sub, to)
-    print(frm, to)
-    hanoi(n - 1, sub, to, frm)
+    if n > 1:
+        hanoi(n-1, 6-a-b, b)
 
+n = int(input())
 
-input_n = int(input())
-print(2**input_n - 1)
-hanoi(input_n, 1, 3, 2)
+print(2**n -1)                               #총 이동해야 하는 횟수
+hanoi(n, 1, 3)
