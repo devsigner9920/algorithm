a59f54c9f3b734f349b0176e2ddb09b769795b75
devsigner <devsigner9920@icloud.com>
2021-10-26 11:14:21 +0900

    commit

--- python/baekjoon/silver/boj_14888.py
+++ python/baekjoon/silver/boj_14888.py
@@ -1,40 +1,26 @@
-def dfs(index,res):
-    global minAns
-    global maxAns
-    # 계산의 끝에 도달했을 때 최댓값과 최솟값이 될 수 있는지 판단한다.
-    if index==N-1:
-        if minAns > res:
-            minAns = res
-        if maxAns < res:
-            maxAns = res
-        return res
-    # 백트래킹 DFS로 순회
-    for i in range(4):
-        temp = res
-        if operator[i]==0:
-            continue
-        if i==0:
-            res+=numArr[index+1]
-        elif i==1:
-            res-=numArr[index+1]
-        elif i==2:
-            res*=numArr[index+1]
-        else:
-            if res<0:
-                res = abs(res)//numArr[index+1]*-1
-            else:
-                res //=numArr[index+1]
-        operator[i] -= 1
-        dfs(index+1,res)
-        operator[i] += 1
-        res = temp
-
 N = int(input())
-numArr = list(map(int,input().split()))
-operator = list(map(int,input().split()))
-minAns = float('Inf')
-maxAns = float('-Inf')
+nums = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 
-dfs(0,numArr[0])
-print(maxAns)
-print(minAns)
+min_, max_ = 1e9, -1e9
+
+def dfs(i, res, add, sub, mul, div):
+    global max_, min_
+    if i == N:
+        max_ = max(res, max_)
+        min_ = min(res, min_)
+        return
+
+    else:
+        if add:
+            dfs(i+1, res+nums[i], add-1, sub, mul, div)
+        if sub:
+            dfs(i+1, res-nums[i], add, sub-1, mul, div)
+        if mul:
+            dfs(i+1, res*nums[i], add, sub, mul-1, div)
+        if div:
+            dfs(i+1, int(res/nums[i]), add, sub, mul, div-1)
+
+dfs(1, nums[0], add, sub, mul, div)
+print(max_)
+print(min_)
